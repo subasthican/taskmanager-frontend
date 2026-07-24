690426e05865e4e7de6cab6f7e43c08735b208ce
subasthican <manoharansubasthican@gmail.com>
2026-07-24 10:44:17 +0530

    refactor: remove search button from TaskPage for cleaner UI

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -114,12 +114,6 @@ const TaskPage = () => {
             onChange={(e)=>setSearch(e.target.value)}
             className="flex-grow p-3 mr-3 border border-gray-300 rounded"
             />
-
-            <button onClick={fetchTasks}
-            className="px-5 py-3 bg-green-600 text-white rounded">
-                search
-            </button>
-            
         </div>
         {/* Form with Category Selector */}
         <form onSubmit={addTask} className="flex mb-8">
