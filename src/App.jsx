0dc315fd5d573a7e67dbb796dfe95f1f5b58ed4a
subasthican <manoharansubasthican@gmail.com>
2026-07-28 16:47:25 +0530

    feat: implement routing and add CompletedTask page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import TaskPage from './pages/TaskPage';
+import CompletedTask from './pages/CompletedTask';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <TaskPage />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={ <TaskPage />} />
+          <Route path="/completed" element={<CompletedTask/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
